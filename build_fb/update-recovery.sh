b145e40e7dea41298fcc6151df991657a5da18b8
bspeng09 <bspeng09@thotakaa.com>
2019-01-25 13:41:22 +0530

    Thotaka Morty Cron Git Update As Of Date : Fri Jan 25 13:41:21 IST 2019
    Signed-off-by: bspeng09 <bspeng09@thotakaa.com>

---
+++ build_fb/update-recovery.sh
@@ -0,0 +1,23 @@
+#!/bin/sh 
+echo "Copying image files to recovery folder "
+cd ~/var-fslc-yocto/sources/meta-thermo-mx6/recipes-qt/tfsapp/files/recovery
+rm -rf SPL 
+rm -rf u-boot.img 
+rm -rf u-boot_gb.img 
+rm -rf tfs-image-rootfs-var-som-mx6.ubi
+rm -rf uImage
+rm -rf uImage-imx6dl-var-som-cap.dtb
+rm -rf uImage-imx6q-var-som-cap.dtb 
+sync
+echo "Sync/Updating image files to recovery folder "
+cp -Lr ~/var-fslc-yocto/build_fb/tmp/deploy/images/var-som-mx6/SPL-nand SPL
+cp -Lr ~/var-fslc-yocto/build_fb/tmp/deploy/images/var-som-mx6/u-boot.img-nand  u-boot.img
+cp -Lr ~/var-fslc-yocto/build_fb/tmp/deploy/images/var-som-mx6/u-boot.img-nand  u-boot_gb.img
+cp -Lr ~/var-fslc-yocto/build_fb/tmp/deploy/images/var-som-mx6/uImage . 
+cp -Lr ~/var-fslc-yocto/build_fb/tmp/deploy/images/var-som-mx6/uImage-imx6q-var-som-cap.dtb .
+cp -Lr ~/var-fslc-yocto/build_fb/tmp/deploy/images/var-som-mx6/uImage-imx6dl-var-som-cap.dtb .
+cp -Lr ~/var-fslc-yocto/build_fb/tmp/deploy/images/var-som-mx6/tfs-image-rootfs-var-som-mx6.ubi .
+cp -Lr ~/var-fslc-yocto/build_fb/tmp/deploy/images/var-som-mx6/tfs-image-minirfs-var-som-mx6.cpio.gz.u-boot .
+chmod +x *
+sync
+
