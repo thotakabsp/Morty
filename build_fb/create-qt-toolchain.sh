1da663d1fa2da7e11aeaa11e4ffd8c020b1e4247
bspeng09 <bspeng09@thotakaa.com>
2019-01-26 06:48:02 +0530

    Thotaka Morty Cron Git Update As Of Date : Sat Jan 26 06:48:02 IST 2019
    Signed-off-by: bspeng09 <bspeng09@thotakaa.com>

--- build_fb/create-qt-toolchain.sh
+++ build_fb/create-qt-toolchain.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 # Create tool chain as SDK for QT Application.
+#
 
+bitbake meta-toolchain-qt5
 
+#tmp/deploy/sdk/fslc-x11-glibc-x86_64-meta-toolchain-qt5-armv7at2hf-neon-toolchain-2.2.1.sh
