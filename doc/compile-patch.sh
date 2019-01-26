2be2fd1754cae619e4c1cddea2b3524bcf9d9d70
bspeng09 <bspeng09@thotakaa.com>
2019-01-26 21:59:46 +0530

    Updated Documents & Readme
    Signed-off-by: bspeng09 <bspeng09@thotakaa.com>

---
+++ doc/compile-patch.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cp ../doc/qv4compiler.cpp  tmp/work/armv7at2hf-neon-fslc-linux-gnueabi/meta-toolchain-qt5/1.0-r0/sdk/image/opt/fslc-framebuffer/2.2.1/sysroots/armv7at2hf-neon-fslc-linux-gnueabi/usr/src/debug/qtdeclarative/5.9.1+gitAUTOINC+525de4ff29-r0/git/src/qml/compiler/
